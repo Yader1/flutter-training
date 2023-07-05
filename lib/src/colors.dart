ba5191f1de71cedfc7cf8c8ef63a856bee69d391
Yader Campbell <yadermejiac123@gmail.com>
2023-07-04 23:16:14 -0600

    feat:App structure

---
+++ lib/src/colors.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+/*
+ * Colores a utilizar en la aplicacion
+ * en caso de querer cambiar el color de la aplicacion solo 
+ * se cambia el codigo Hexadecimal del color
+ */
+
+class AppColors {
+  static Color homePageBackgroud = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xFF0f17ad);
+  static Color gradientSecond = const Color(0xFF6985e8);
+  static Color homePageTitle = const Color(0xFF302f51);
+  static Color homePageSubtitle = const Color(0xFF414160);
+  static Color homePageDetail = const Color(0xFF6588f4);
+  static Color homePageIcons = const Color(0xFF3b3c5c);
+  static Color homePageContainerTextSmall = const Color(0xFFf4f5fd);
+  static Color homePageContainerTextBig = const Color(0xFFffffff);
+  static Color homePagePlanColor = const Color(0xFFa2a2b1);
+  static Color secondPageTopIconColor = const Color(0xFFb7bce8);
+  static Color secondPageTitleColor = const Color(0xFFfefeff);
+  static Color secondPageContainerGradient1stColor = const Color(0xFF6588f4);
+  static Color secondPageContainerGradient2stColor = const Color(0xFF6588f4);
+  static Color secondPagePageIconColor = const Color(0xFFfafafe);
+  static Color loopColor = const Color(0xFF6d8dea);
+  static Color setsColor = const Color(0xFF9999a9);
+  static Color circuitsColor = const Color(0xFF2f2f51);
+}
